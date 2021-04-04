6d5dcd90191008b01fca6d1bfe8e9ce80c4f4bd5
suzuki pri <pg.econ4869@gmail.com>
2021-04-04 18:33:21 +0900

    mongo dbにコネクションする

--- index.js
+++ index.js
@@ -4,25 +4,43 @@ const express = require('express')
 const expressPlayground = require('graphql-playground-middleware-express').default
 const { readFileSync } = require('fs')
 
+const { MongoClient } = require('mongodb')
+require('dotenv').config()
+
 const typeDefs = readFileSync('./typeDefs.graphql', 'UTF-8')
 const resolvers = require('./resolvers')
 
-// Express アプリケエーションを作成する
-var app = express()
+async function start() {
+  // Express アプリケエーションを作成する
+  var app = express()
 
-// サーバーのインスタンスを作成
-// その際、typeDefs(スキーマ)とリゾルバを引数に取る
-const server = new ApolloServer({
-  typeDefs,
-  resolvers
-})
+  const MONGO_DB = process.env.DB_HOST
 
-// Express にミドルウェアを追加する
-server.applyMiddleware({ app })
+  const client = await MongoClient.connect(
+    MONGO_DB,
+    { useNewUrlParser: true }
+  )
+  const db = client.db()
 
-// ホームルートを作成する
-app.get('/', (_, res) => res.end('Welcome to the PhotoShare API'))
-app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+  const context = { db }
 
-// 特定のポートでリッスンする
-app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`))
+  // サーバーのインスタンスを作成
+  // その際、typeDefs(スキーマ)とリゾルバを引数に取る
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context
+  })
+
+  // Express にミドルウェアを追加する
+  server.applyMiddleware({ app })
+
+  // ホームルートを作成する
+  app.get('/', (_, res) => res.end('Welcome to the PhotoShare API'))
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }))
+
+  // 特定のポートでリッスンする
+  app.listen({ port: 4000 }, () => console.log(`GraphQL Server running @ http://localhost:4000${server.graphqlPath}`))
+}
+
+start()
